bcf71d1b75cb90ebfbc3899e5590b732f0690dcf
Laneer40 <ltcbandli98@gmail.com>
2017-04-06 23:19:02 -0400

    Update
    4/6/2017 Nighpush

--- MVC_Portfolio/Controllers/HomeController.cs
+++ MVC_Portfolio/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Blog2.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +16,28 @@ namespace MVC_Portfolio.Controllers
         {
             ViewBag.Title = "Main Page";
             return View();
-        }        
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold> ({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = "MyPortfolio<ltcbandli98@gmail.com>";
+                    model.Body = "This is a message from your Portfolio site. The name and the email of the contacting person is above.";
+
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"]) { Subject = "Portfolio Contact Email", Body = string.Format(body, model.FromName, model.FromEmail, model.Body), IsBodyHtml = true };
+
+                    var svc = new PersonalEmail(); await svc.SendAsync(email);
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); await Task.FromResult(0); }
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
